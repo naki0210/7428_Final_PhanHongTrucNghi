8bdb99b614b89f7f888f328528471b46bdfab495
naki0210 <myhangtran256@gmail.com>
2025-08-13 20:39:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 # app.py
 import streamlit as st
-
-pandas
-numpy
-matplotlib
-seaborn
-scikit-learn
-
-import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
